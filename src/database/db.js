b41944fb471499a6b58e9064ffdec4339cb2589f
Luan Rodrigues <luanrodriguesep@yahoo.com.br>
2020-06-10 07:05:38 -0300

    form com banc
    Arquivos configurados pós formulario ser integrado ao banco de dados

--- src/database/db.js
+++ src/database/db.js
@@ -10,28 +10,28 @@ const sqlite3 = require("sqlite3").verbose()
 //  ou pode ser feito com esse comando tbm
 
 const db = new sqlite3.Database("./src/database/database.db") // constructor ou classe
+module.exports = db
 
-
-/* // vamos utilizar o objeto de banco de dados, para  nossas operações
+// vamos utilizar o objeto de banco de dados, para  nossas operações
 
 db.serialize(() => {  // uma function atrelada aum objto, será considerado metodo
     // Com comandos SQL eu vou:
 
     // 01 - criar uma tabela
-    db.run(`
-        CREATE TABLE IF NOT EXISTS places (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            image TEXT, 
-            name TEXT,
-            address TEXT,
-            address2 TEXT,
-            state TEXT,
-            city TEXT,
-            items TEXT
-        );
-    `)
+    /* db.run(`
+       CREATE TABLE IF NOT EXISTS places (
+           id INTEGER PRIMARY KEY AUTOINCREMENT,
+           image TEXT, 
+           name TEXT,
+           address TEXT,
+           address2 TEXT,
+           state TEXT,
+           city TEXT,
+           items TEXT
+       );
+   `) 
     // 02 - iserir dados na tabela 
-    const query = `
+     const query = `
     INSERT INTO places (
         image, 
         name,
@@ -44,13 +44,13 @@ db.serialize(() => {  // uma function atrelada aum objto, será considerado meto
 `
 
     const values = [
-        "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-        "Papersider",
+        "https://images.unsplash.com/flagged/photo-1566843017081-a27f11f2bf05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+        "Colectoria",
         "Guilherme Gembala, Jardim América",
         "Numero 260",
         "Santa Catarina",
         "Rio do Sul",
-        "Papaeis e Papelão",
+        "Residuos Eletronicos e Lampadas"
     ]
 
     function afterInsertData(err) {
@@ -62,7 +62,7 @@ db.serialize(() => {  // uma function atrelada aum objto, será considerado meto
         console.log(this)
     }
 
-    db.run(query, values, afterInsertData) // callback (chamar de volta)
+    db.run(query, values, afterInsertData)  // callback (chamar de volta)
 
 
 
@@ -71,24 +71,38 @@ db.serialize(() => {  // uma function atrelada aum objto, será considerado meto
 
     // 03 - consultar dados da tabela
 
-    db.all(` SELECT * FROM places`, function (err, rows) {
+     db.all(` SELECT * FROM places`, function (err, rows) {
+         if (err) {
+             return console.log(err)
+         }
+ 
+         console.log("Aqui está seus registros")
+         console.log(rows)
+     })  */
+
+    // 04 - Deletar um dado da tabela
+
+     /*db.run(`DELETE  FROM places WHERE id = ?`, [12], function (err) {  // por que (1) um ? porque so temos um registro na tabela, dentro da array vai o numero do registro 
+         if (err) {
+             return console.log(err)
+         }
+         console.log("Registro deletado com sucessso!")
+     }) */
+
+
+
+    //    consultar registros 
+
+    /* db.all(` SELECT * FROM places`, function (err, rows) {
         if (err) {
             return console.log(err)
         }
 
         console.log("Aqui está seus registros")
         console.log(rows)
-    })
-
-    // 04 - Deletar um dado da tabela
-
-    /*db.run(`DELETE  FROM places WHERE id = ?`, [1], function (err) {  // por que (1) um ? porque so temos um registro na tabela, dentro da array vai o numero do registro 
-        if (err) {
-            return console.log(err)
-        }
-        console.log("Registro deletado com sucessso!")
-    })*/  
-//})
+    }) */
 
 
- //*/ 
+})
+
+
--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const server = express()
 
+// Pegar o banco de Dados
+
+const db = require("./database/db") // variavel nao preqisava propriamente ser "db" poderia ser qualquer outro nome
+
 //configurar pasta publica
 // queremos que esta pasta fique como uma pasta comum, como se o conteudo estivesse disponivel na web
 server.use(express.static("public"))
@@ -9,8 +13,8 @@ server.use(express.static("public"))
 
 //configurar pas publica 
 const nunjucks = require("nunjucks") // Nunjucks um motor de html pra deixar ele mais inteligente como uma linguagem, com condições e variaveis
-nunjucks.configure("src/views",{
-    express :server,
+nunjucks.configure("src/views", {
+    express: server,
     noCache: true
 })
 
@@ -19,37 +23,59 @@ nunjucks.configure("src/views",{
 
 // configurar caminhos da minha aplicação
 //pagina inicial
-//rec = requisiçao
+//req = requisiçao
 //res = resposta
 /*
 // configuração pre nunjuncks
 
 
-server.get("/", function (rec, res){ 
-    res.sendFile(__dirname + "/views/index.html")//dirner variavel global, ja vem pr padrao nao precisa ser definida
+server.get("/", function (req, res){ 
+    res.sendFile(__dirname + "/views/index.html")//dirner variavel global, ja vem pr padrao nao preqisa ser definida
 })
 
-server.get("/create.point", function (rec, res){ 
+server.get("/create.point", function (req, res){ 
     res.sendFile(__dirname + "/views/create.point.html")//dirner variavel global, ja vem pr padrao nao precisa ser definida
 })
 
-server.get("/search-results", function (rec, res){ 
-    res.sendFile(__dirname + "/views/search-results.html")//dirner variavel global, ja vem pr padrao nao precisa ser definida
+server.get("/search-results", function (req, res){ 
+    res.sendFile(__dirname + "/views/search-results.html")//dirner variavel global, ja vem pr padrao nao preqisa ser definida
 })*/
 
 
-    // Configuração pós nunjucks
+// Configuração pós nunjucks
 
-server.get("/", function (rec, res){ 
-    return res.render("index.html", { title: "Seu marketplace de coletas de residuos"})
+server.get("/", function (req, res) {
+    return res.render("index.html", { title: "Seu marketplace de coletas de residuos" }) 
 })
 
-server.get("/create.point", function (rec, res){ 
+
+
+
+server.get("/create.point", function (req, res) {
+    //req.query pegar informações de um formulário : Query strings da nossa Url
+    console.log(req.query)
+    
     return res.render("create.point.html")
 })
 
-server.get("/search-results", function (rec, res){ 
-    return res.render("search-results.html")
+
+
+
+server.get("/search-results", function (req, res) {
+
+    // ?Pegar o dados do banco de Dados
+    db.all(` SELECT * FROM places`, function (err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+
+        const total = rows.length
+        console.log("Aqui está seus registros") // consolo.log nos mostrar informações dos rows no terminal, deixa-lo aqui é opcional
+        console.log(rows)
+        // Mostrar a pagina html com os do banco de dados
+        return res.render("search-results.html",{ places: rows, total: total})
+    })
+    
 })
 
 //ligar o servidor 
